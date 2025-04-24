31ab366a403602839ae0115f7d1f4166f52204cd
travinne <travinne.mutahi@moringaschool.com>
2025-04-24 21:23:27 +0300

    added new header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>
+    <div className='header'>
         <h1> weather app</h1>
-        <h4> see the current weather conditions from all over the world</h4>
+        <h4> see the current weather acroos the world</h4>
     </div>
   )
 }
